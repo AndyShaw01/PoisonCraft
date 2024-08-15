64ce0994ede2e14b3f1cef102620637ae9b593a9
AndyShaw01 <974052646@qq.com>
2024-08-15 22:31:31 +0800

    update exp setting

--- Experiments/gcg_exp.py
+++ Experiments/gcg_exp.py
@@ -23,7 +23,8 @@ if __name__ == "__main__":
     parser.add_argument("--add_eos", action='store_true')
     parser.add_argument("--eos_num", type=int, default=10)
     parser.add_argument("--run_index", type=int, default=1)
-    parser.add_argument("--loss_threshold", type=float, default=0.5)
+    parser.add_argument("--loss_threshold", type=float, default=0.4)
+    # parser.add_argument("--random", type=bool, default=False)
 
     args = parser.parse_args()
     
