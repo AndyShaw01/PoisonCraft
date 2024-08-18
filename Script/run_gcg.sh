cb2ed280f07c3f2d0347e58706c27de877f372ad
AndyShaw01 <974052646@qq.com>
2024-08-18 17:46:33 +0800

    update bash script

--- Script/run_gcg.sh
+++ Script/run_gcg.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+MODEL="t5-base"
+# MODEL="MPNetModel"
 PYTHON_SCRIPT="Experiments/gcg_exp.py"
-MODEL_PATH="/data1/shaoyangguang/offline_model/SBERT"
 ADD_EOS=False
 RUN_INDEX=1
 INDEX=0
+if [ "$MODEL" = "t5-base" ]; then
+    LOSS_THRESHOLD=0.05
+    MODEL_PATH="/data1/shaoyangguang/offline_model/t5-base"
+elif [ "$MODEL" = "MPNetModel" ]; then
+    LOSS_THRESHOLD=0.1
+    MODEL_PATH="/data1/shaoyangguang/offline_model/MPNetModel"
+fi
 
 if [ "$ADD_EOS" = "True" ]; then
     LOG_PATH="Logs/${MODEL_PATH}/GCG_eos-${RUN_INDEX}"
@@ -20,5 +28,10 @@ if [ "$ADD_EOS" = "True" ]; then
     ADD_EOS_FLAG="--add_eos"
 fi
 
+if [ "$MODEL" = "t5-base" ]; then
+    python -u "$PYTHON_SCRIPT" --index $INDEX --model_path $MODEL_PATH $ADD_EOS_FLAG --run_index $RUN_INDEX --loss_threshold $LOSS_THRESHOLD > "$LOG_PATH/gcg_5*.log" 2>&1
+elif [ "$MODEL" = "MPNetModel" ]; then
+    python -u "$PYTHON_SCRIPT" --index $INDEX --model_path $MODEL_PATH $ADD_EOS_FLAG --run_index $RUN_INDEX --loss_threshold $LOSS_THRESHOLD > "$LOG_PATH/gcg_5!.log" 2>&1
+fi
 
-python -u "$PYTHON_SCRIPT" --index $INDEX --model_path $MODEL_PATH $ADD_EOS_FLAG --run_index $RUN_INDEX > "$LOG_PATH/gcg_${INDEX}.log" 2>&1
+# python -u "$PYTHON_SCRIPT" --index $INDEX --model_path $MODEL_PATH $ADD_EOS_FLAG --run_index $RUN_INDEX
