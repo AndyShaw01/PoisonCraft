1b9b51272f23d7cac1a52b4292076c76379ab545
AndyShaw01 <974052646@qq.com>
2024-08-18 17:45:31 +0800

    update params

--- Experiments/gcg_exp.py
+++ Experiments/gcg_exp.py
@@ -15,15 +15,15 @@ if __name__ == "__main__":
                         help='target model path')
     parser.add_argument("--control_string_length", type=int, default=20) # check: 20*, Llama: 20! control string length
     parser.add_argument("--max_attack_steps", type=int, default=500)     # 
-    parser.add_argument("--early_stop", type=bool, default=False)        # 
+    parser.add_argument("--early_stop", type=bool, default=True)        # 
     parser.add_argument("--max_steps", type=int, default=500)            # 
-    parser.add_argument("--max_attack_attempts", type=int, default=1)    # rerun, try, if >20, 30 -> rerun
+    parser.add_argument("--max_attack_attempts", type=int, default=10)    # rerun, try, if >20, 30 -> rerun
     parser.add_argument("--max_prompts_in_single_attack", type=int, default=1) 
     parser.add_argument("--max_successful_prompt", type=int, default=1)  
     parser.add_argument("--add_eos", action='store_true')
     parser.add_argument("--eos_num", type=int, default=10)
     parser.add_argument("--run_index", type=int, default=1)
-    parser.add_argument("--loss_threshold", type=float, default=0.4)
+    # parser.add_argument("--loss_threshold", type=float, default=0.1)
     # parser.add_argument("--random", type=bool, default=False)
 
     args = parser.parse_args()
