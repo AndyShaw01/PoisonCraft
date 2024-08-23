eb80f490c69187377083d7dfc20cd2f6a541ca4d
AndyShaw01 <974052646@qq.com>
2024-08-23 16:09:59 +0800

    add scrpt: get top-k threshold

--- Script/run_process_data.sh
+++ Script/run_process_data.sh
@@ -4,6 +4,8 @@ PYTHON_SCRIPT="Experiments/data_process.py"
 
 LOG_PATH="Logs/tmp_data/"
 
+TOPK=99
+
 mkdir -p "$LOG_PATH"
 
-python -u "$PYTHON_SCRIPT" > "$LOG_PATH/get_ground_truth.log" 2>&1
+python -u "$PYTHON_SCRIPT" --k $TOPK > "$LOG_PATH/get_ground_truth.log" 2>&1
