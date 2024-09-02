e56cbc9708e0133499a3cfcd908fb53f3a3da8d2
AndyShaw01 <974052646@qq.com>
2024-09-02 17:33:52 +0800

    fix error: gpu 4-> gpu 0

--- Script/run_gcg_category.sh
+++ Script/run_gcg_category.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 BASH_SCRIPT="./Script/run_gcg_rag.sh"
-GROUP_LIST="1,2,3,4,5,6,7"
-GPU_LIST="1,2,3,4"
+GROUP_LIST="1,2,3,4,5,6,7,8,9,10,11,12,13,14"
+GPU_LIST="0,1,2,3"
 for GROUP_NUM in $(echo $GROUP_LIST | sed "s/,/ /g")
 do
     if [ "$GROUP_NUM" = "1" ]; then
@@ -12,13 +12,27 @@ do
     elif [ "$GROUP_NUM" = "3" ]; then
         GPU_NUM=3
     elif [ "$GROUP_NUM" = "4" ]; then
-        GPU_NUM=4
+        GPU_NUM=0
     elif [ "$GROUP_NUM" = "5" ]; then
         GPU_NUM=1
     elif [ "$GROUP_NUM" = "6" ]; then
         GPU_NUM=2
     elif [ "$GROUP_NUM" = "7" ]; then
         GPU_NUM=3
+    elif [ "$GROUP_NUM" = "8" ]; then
+        GPU_NUM=0
+    elif [ "$GROUP_NUM" = "9" ]; then
+        GPU_NUM=1
+    elif [ "$GROUP_NUM" = "10" ]; then
+        GPU_NUM=2
+    elif [ "$GROUP_NUM" = "11" ]; then
+        GPU_NUM=3
+    elif [ "$GROUP_NUM" = "12" ]; then
+        GPU_NUM=0
+    elif [ "$GROUP_NUM" = "13" ]; then
+        GPU_NUM=1
+    elif [ "$GROUP_NUM" = "14" ]; then
+        GPU_NUM=2
     fi
     bash $BASH_SCRIPT $GROUP_NUM $GPU_NUM &
     echo "Group $GROUP_NUM is running on GPU $GPU_NUM"
