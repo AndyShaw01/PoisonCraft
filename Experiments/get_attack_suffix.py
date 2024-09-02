5bfc17299e075949cc82222b474df9ce15dc1b9f
AndyShaw01 <974052646@qq.com>
2024-09-02 11:35:56 +0800

    add is_nan

--- Experiments/get_attack_suffix.py
+++ Experiments/get_attack_suffix.py
@@ -5,16 +5,26 @@ import json
 import torch   
 import argparse
 import pandas as pd
+import numpy as np
 import torch.nn.functional as F
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 
 from SentenceEmbedding.SentenceEmbedding import SentenceEmbeddingModel
 
+def is_nan(value):
+    try:
+        return np.isnan(value)
+    except TypeError:
+        # 如果类型不支持 isnan，则返回 False
+        return False
+
 def main(args):
     df = pd.read_csv(args.candidate_file)
     for i in range(len(df)):
         attack_control_suffix = df['control_suffix'].iloc[i]
+        if is_nan(attack_control_suffix):
+            continue
         df_queries = pd.read_json(args.queries_file, lines=True)
         questions = df_queries['text'].tolist()
 
@@ -28,13 +38,13 @@ def main(args):
         questions_embedding = similarity_model(questions)
 
         similarity = torch.matmul(sentence_embedding, questions_embedding.t())
-        print(similarity)
+        # print(similarity)
         # 记录similarity中第二大的值
-        # secend_max = torch.topk(similarity[0], 2).values[1]
+        secend_max = torch.topk(similarity[0], 8).values[7]
         # 记录similarity中最大的值
         # max_max = torch.topk(similarity[0], 1).values[0]
         # # print(f"{i}, {secend_max}")
-        # print(f"{i}, {max_max}")
+        print(f"{i}, {secend_max}")
         # # 判断，如果 similarity中若有大于0.9的值，则此时，记录下来
         # if len(similarity > 0.9)>3:
         #     print("The index is: ", i)
@@ -44,8 +54,8 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Get attack suffix')
-    parser.add_argument('--candidate_file', type=str, default='./Results/pre_exp/group/results_top_10.csv', help='The candidate file')
-    parser.add_argument('--queries_file', type=str, default='./Dataset/nq/category/categorized_jsonl_files/category_1.jsonl', help='The queries file')
+    parser.add_argument('--candidate_file', type=str, default='./Results/improve_exp_0830/batch-4/category_6/results_top_10.csv', help='The candidate file')
+    parser.add_argument('--queries_file', type=str, default='./Dataset/nq/category/categorized_jsonl_files/category_6.jsonl', help='The queries file')
 
     args = parser.parse_args()
 
