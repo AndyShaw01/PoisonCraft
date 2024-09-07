6280a57cf74e71b78ddf353afe0f54518420070f
AndyShaw01 <974052646@qq.com>
2024-09-07 16:07:50 +0800

    update

--- Script/run_data_process.sh
+++ Script/run_data_process.sh
@@ -7,8 +7,8 @@ for GROUP_INDEX in $(echo $GROUP_LIST | sed "s/,/ /g")
 do
     for TOP_K in $(echo $TOPK_LIST | sed "s/,/ /g")
     do
-        TEST_FILE="./Dataset/nq/category/categorized_jsonl_files_14_test/category_${GROUP_INDEX}.jsonl"
-        LOG_PATH_PRE="Logs/tmp_data/GCG-Batch-${ATTACK_BATCH_SIZE}-${GROUP_MODE}-category_${GROUP_INDEX}-test"
+        TEST_FILE="./Dataset/nq/category/categorized_jsonl_files_14_test_recheck/category_${GROUP_INDEX}.jsonl"
+        LOG_PATH_PRE="Logs/tmp_data/GCG-Batch-${ATTACK_BATCH_SIZE}-${GROUP_MODE}-category_${GROUP_INDEX}-test-recheck"
         mkdir -p "$LOG_PATH_PRE"
         python -u "$PYTHON_PRE_SCRIPT" --k $TOP_K --category $GROUP_INDEX --train_queries_path $TEST_FILE> "$LOG_PATH_PRE/get_ground_truth_${TOP_K}.log" 2>&1
     done
