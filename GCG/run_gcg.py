c60b2b3b3fee94538b5298b08dd11bd470ef5266
AndyShaw01 <974052646@qq.com>
2024-09-24 21:43:32 +0800

    update running setting: remove groundtruth

--- GCG/run_gcg.py
+++ GCG/run_gcg.py
@@ -31,21 +31,17 @@ def gcg_attack_all(args):
             queries_id.append(data['_id'])
             queries_text.append(data['text'])
     
-    ground_truth = pd.read_csv(f'./Dataset/nq/ground_truth_train_recheck_0905/ground_truth_top_{args.topk}_category_{args.group_index}.csv')
     if args.attack_batch_size > 1:
-        args.save_path = f"./Results/improve_exp/batch-{args.attack_batch_size}/category_{args.group_index}/results_top_{args.topk}.csv"
+        args.save_path = f"./Results/improve_gcg/batch-{args.attack_batch_size}/category_{args.group_index}/results_top_{args.topk}.csv"
     else:
-        args.save_path = f"./Results/improve_exp/single/results_top_{args.topk}.csv"
+        args.save_path = f"./Results/improve_gcg/single/results_top_{args.topk}.csv"
     print("The save path is: ", args.save_path)
+
     if not os.path.exists(os.path.dirname(args.save_path)):
         os.makedirs(os.path.dirname(args.save_path))
+
     gcg = GCG(args)
     for i in range(len(queries_id)//args.attack_batch_size):
         args.index = i
         gcg.question = queries_text[i*args.attack_batch_size:(i+1)*args.attack_batch_size]
-        # 按照batch获取ground truth，最终求均值
-        # gcg.ground_truth = ground_truth[ground_truth['test_name'] == queries_id[i*args.attack_batch_size:(i+1)*args.attack_batch_size]].values[0][1]
-        
-        gcg.product_threshold = ground_truth[ground_truth['test_name'].isin(queries_id[i*args.attack_batch_size:(i+1)*args.attack_batch_size])][f'matched_bar_{args.topk}'].values
-        # gcg.product_threshold = ground_truth[ground_truth['test_name'] == queries_id[i]].values[0][1]
         gcg.run(args.target)
