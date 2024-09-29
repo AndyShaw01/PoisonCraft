0f0df1a7f82e34d040bbd771cbc42934b0c547fd
AndyShaw01 <974052646@qq.com>
2024-09-29 16:24:23 +0800

    update

--- Script/run_cross_attack.sh
+++ Script/run_cross_attack.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+export all_proxy=socks5://192.168.112.1:7890 
+export CUDA_VISIBLE_DEVICES=2
+
 PYTHON_SCRIPT="Experiments/cross_attack.py"
 
 LOG_PATH="Logs/cross_attack"
 mkdir -p "$LOG_PATH"
 
-export CUDA_VISIBLE_DEVICES=1
-
-python -u $PYTHON_SCRIPT > $LOG_PATH/recheck.log 2>&1 &
+python -u $PYTHON_SCRIPT # > $LOG_PATH/recheck.log # 2>&1 &
--- Script/run_gcg_category.sh
+++ Script/run_gcg_category.sh
@@ -3,21 +3,31 @@
 BASH_SCRIPT="./Script/run_gcg_rag.sh"
 # GROUP_LIST="1,2,3,4,5,6,7,8,9,10,11,12,13,14"
 GROUP_NUM="1"
-CONTROL_LENGTH_LIST="50,55,60,65,70,75"
+CONTROL_LENGTH_LIST="50,55,60,65,70,75,80,85,90,95,100"
 GPU_LIST="0,1,2,3"
 for CONTROL_LENGTH in $(echo $CONTROL_LENGTH_LIST | sed "s/,/ /g")
 do
     if [ "$CONTROL_LENGTH" = "50" ]; then
-        GPU_NUM=1
-    elif [ "$CONTROL_LENGTH" = "55" ]; then
         GPU_NUM=2
-    elif [ "$CONTROL_LENGTH" = "60" ]; then
+    elif [ "$CONTROL_LENGTH" = "55" ]; then
         GPU_NUM=3
+    elif [ "$CONTROL_LENGTH" = "60" ]; then
+        GPU_NUM=1
     elif [ "$CONTROL_LENGTH" = "65" ]; then
         GPU_NUM=1
-    elif [ "$CONTROL_LENGTH" = "60" ]; then
+    elif [ "$CONTROL_LENGTH" = "70" ]; then
         GPU_NUM=2
     elif [ "$CONTROL_LENGTH" = "75" ]; then
+        GPU_NUM=3
+    elif [ "$CONTROL_LENGTH" = "80" ]; then
+        GPU_NUM=2
+    elif [ "$CONTROL_LENGTH" = "85" ]; then
+        GPU_NUM=1
+    elif [ "$CONTROL_LENGTH" = "90" ]; then
+        GPU_NUM=2
+    elif [ "$CONTROL_LENGTH" = "95" ]; then
+        GPU_NUM=3
+    elif [ "$CONTROL_LENGTH" = "100" ]; then
         GPU_NUM=3
     fi
     bash $BASH_SCRIPT $GROUP_NUM $CONTROL_LENGTH $GPU_NUM &
