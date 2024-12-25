5b7cbc9b386f926386a3cd4c4d0fbf5e9bd26ee2
AndyShaw01 <974052646@qq.com>
2024-12-25 16:23:44 +0800

    🔴 scripts/run_data_process.sh 🛠️ scripts/run_prepare_dataset.sh -> Simplified dataset preparation 🛠️ scripts/run_process_data.sh -> Updated data processing logic

--- scripts/run_prepare_dataset.sh
+++ scripts/run_prepare_dataset.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-PYTHON_SCRIPT_DOWNLOAD_DATASET="experiments/download_dataset.py"
-
-# download dataset
-
-
-# extract the beir test dataset
-
-# extract the shadow dataset and the test dataset
-
-# get the domain of the queries in the shadow dataset
 export all_proxy=socks5://192.168.112.1:7890
 
-PYTHON_SCRIPT="Experiments/classify_queries_by_domain.py"
+PYTHON_SCRIPT_PREPARE_DATASET="experiments/prepare_datasets.py"
+PYTHON_SCRIPT_CLASSIFY_QUERIES="experiments/classify_queries_by_domain.py"
+
 LOG_PATH="Logs/classify_queries"
 MODE="test"
-DATASET="hotpotqa"
+DATASETS=("nq" "msmarco" "hotpotqa")
 
-mkdir -p "$LOG_PATH" 
+mkdir -p "$LOG_PATH"
 
-python -u $PYTHON_SCRIPT --mode $MODE --dataset $DATASET > $LOG_PATH/classify_queries_${DATASET}_${MODE}.log 2>&1
+# Run prepare_datasets.py
+python -u $PYTHON_SCRIPT_PREPARE_DATASET > $LOG_PATH/prepare_datasets.log 2>&1
+
+# Run classify_queries_by_domain.py for each dataset
+for DATASET in "${DATASETS[@]}"; do
+    python -u $PYTHON_SCRIPT_CLASSIFY_QUERIES --mode $MODE --dataset $DATASET > $LOG_PATH/classify_queries_${DATASET}_${MODE}.log 2>&1
+done
--- scripts/run_process_data.sh
+++ scripts/run_process_data.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
+PYTHON_PRE_SCRIPT="experiments/process_data.py"
+DOMAIN_LIST="1,2,3,4,5,6,7,8,9,10,11,12,13,14"
+TOPK_LIST=4,9,19,49
+TARGET_DATASET="nq" # msmarco, hotpotqa
+MODE="test" # test
+RETRIEVER="contriever" # contriever ance
+for DOMAIN_INDEX in $(echo $DOMAIN_LIST | sed "s/,/ /g")
+do
+    for TOP_K in $(echo $TOPK_LIST | sed "s/,/ /g")
+    do
+        TARGET_FILE="./Datasets/${TARGET_DATASET}/domain/${MODE}_domains_14/domain_${GROUP_INDEX}.jsonl"
+        echo "Processing $TARGET_FILE"
+        LOG_PATH_PRE="Logs/main_result/${RETRIEVER}/${TARGET_DATASET}/${TARGET_DATASET}-${MODE}"
+        mkdir -p "$LOG_PATH_PRE"
+        python -u "$PYTHON_PRE_SCRIPT" --dataset $TARGET_DATASET \
+            --k $TOP_K --domain $DOMAIN_INDEX --target_queries_path $TARGET_FILE --retriever $RETRIEVER # > "$LOG_PATH_PRE/get_ground_truth_${TOP_K}_.log" 2>&1
+    done
+done
 
-PYTHON_SCRIPT="Experiments/data_process.py"
-
-LOG_PATH="Logs/tmp_data/"
-
-TOPK=99
-
-mkdir -p "$LOG_PATH"
-
-python -u "$PYTHON_SCRIPT" --k $TOPK > "$LOG_PATH/get_ground_truth.log" 2>&1
+# 等待所有后台任务完成
+wait
